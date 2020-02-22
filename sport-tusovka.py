a1ba40404ed6f92d40f8dda3fe4b90206e34f195
Mykola Lev <udent@protonmail.com>
2020-02-22 16:41:29 +0200

    startuem!

---
+++ sport-tusovka.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+from base64 import b64encode, b64decode
+import requests
+import json
+import secrets
+import os, stat
+from re import sub
+import ssl
+
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
+
+app.config['SECRET_KEY'] = os.environ['FLASK_KEY']
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_audio():
+    emit('helloworld')
+    return 'helloworld'
+
+@app.route('/easter_egg')
+def easter_egg():
+    return "ну вітаю, маладєц, шо скажеш, круто! умєєш, магьош"
+
+if __name__ == '__main__':
+    socketio.run(app)
